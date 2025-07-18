afc1b10032612135414802d90e5314c11923b072
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-07-18 15:09:52 -0500

    Updated modules

--- scripts/fishList.js
+++ scripts/fishList.js
@@ -10,8 +10,10 @@ export const fishList = () => {
                 <img src="${fish.image}" alt="${fish.species} image" class="fish_image">
                 <div class="fish_details">
                     <h2 class="fish_name">${fish.name}</h2>
-                    <p class="fish_species">Species:${fish.species}</p>
+                    <p class="fish_species">Species: ${fish.species}</p>
                     <p class="fish_length">Length: ${fish.length} inches</p>
+                    <p class="fish_location">Location: ${fish.location}</p>
+                    <p class="fish_diet">Diet: ${fish.diet}</p>
                 </div>
             </article>
         `
