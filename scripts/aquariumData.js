577bdd25d001bae2c9ec2b3f29e35724ba315620
Thomas Robert Castellon <thomas.castellon@gmail.com>
2025-07-18 09:48:00 -0500

    Updated Aquarium data module

--- scripts/aquariumData.js
+++ scripts/aquariumData.js
@@ -78,5 +78,46 @@ export const database = {
             topic: "Other considerations",
             text: "Avoid touching the fish, loud noises and vibrations, don't overcrowd the tank, and ensure compatibility of species to prevent aggression."
         }
+    ],
+    locations: [
+        {
+            name: "Great Barrier Reef",
+            country: "Australia",
+            description: "The Great Barrier Reef is the world's largest coral reef system, located off the northeastern coast of Australia. It stretches over 2,300 kilometers (1,400 miles) and is composed of nearly 3,000 individual reefs and 600 islands."
+        },
+        {
+            name: "South Pacific",
+            country: "Guam, New Zealand, Samoa, and serveral more",
+            description: "The South Pacific Ocean is a vast body of water located south of the equator, extending towards Antarctica. It's characterized by deep basins, oceanic ridges, and a prominent subtropical gyre. The South Pacific is also known for its high biological productivity due to upwelling and strong currents."
+        },
+        {
+            name: "Indian Ocean",
+            country: "India, Indonesia, Somalia, Saudi Arabia, and several more",
+            description: "The Indian Ocean is the third-largest of the world's five oceans, covering about 20% of the Earth's surface. It is located south of Asia, east of Africa, west of Australia, and north of Antarctica. The ocean is known for its complex geological structure, warm waters, and significant impact on global climate."
+        },
+        {
+            name: "Western Atlantic Ocean",
+            country: "United States, Mexico, Cuba, Dominican Republic, and several more",
+            description: "The western part of the Atlantic Ocean, particularly the North Atlantic, is characterized by its continental shelf, deep trenches, and significant currents. The ocean's western side also includes the Caribbean Sea, a distinct region with its own unique features and currents."
+        },
+        {
+            name: "Indo-Pacific",
+            country: "Australia, India, Japan, South Korea, and the countries of ASEAN (Association of Southeast Asian Nations). ",
+            description: "The Indo-Pacific is a vast biogeographic region encompassing the tropical waters of the Indian and Pacific Oceans, along with the connecting seas. It's particularly relevant in marine biology and related fields due to the interconnectedness of many marine habitats across this area."
+        },
+        {
+            name: "Central Pacific",
+            country: "Kiribati, Nauru, Tuvalu, the Marshall Islands, Micronesia, Palau, Fiji, and Samoa.",
+            description: "The central Pacific Ocean is a vast and geologically stable area characterized by expansive underwater plains, numerous seamounts, and volcanic activity. It lies between the eastern and western regions of the Pacific and is known for its deep ocean floor and unique geological formations."
+        },
+        {
+            name: "Caribbean Sea",
+            country: "Bahamas, Cuba, Dominican Republic, Jamaica, Puerto Rico, and several more",
+            description: "The Caribbean Sea is a large tropical sea in the Western Atlantic Ocean, known for its beautiful turquoise waters, vibrant coral reefs, and numerous islands. The sea is renowned for its rich biodiversity and is a popular destination for tourism, watersports, and fishing."
+        },{
+            name: "Gulf of Mexico",
+            country: "The United States, Mexico, and Cuba",
+            description: "Surrounded by the United States, Mexico and Cuba, the Gulf of Mexico is 600,000 square miles of warm, sunlit waters and a steady food supply for an array of fish, wildlife and humans alike. The Gulf is a semi-enclosed sea that connects to the Atlantic Ocean through the Florida Straits and the Yucatán Channel."
+        }
     ]
 }
